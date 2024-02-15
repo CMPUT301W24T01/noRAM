731a230f15c22bf0b09eb9241057dd31cb744033
Christiaan <clventer@ualberta.ca>
2024-02-15 11:35:37 -0700

    added database dependencies and base level interface

--- code/noRAM/app/build.gradle.kts
+++ code/noRAM/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -39,4 +40,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Firebase BOM dependency
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    // Firebase Firestore dependency
+    implementation("com.google.firebase:firebase-firestore")
+
 }
