3555acb2a5fccd2567342a64d92ddc58f53b5fdc
gdumouli <90409961+gdumouli@users.noreply.github.com>
2024-02-23 15:12:46 -0700

    Starting event info page

---
+++ code/noRAM/app/src/main/java/com/example/noram/AttendeeEventInfo.java
@@ -0,0 +1,13 @@
+package com.example.noram;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AttendeeEventInfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_attendee_info);
+    }
+}
