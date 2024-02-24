9d7917c2392e1845a0267e74bcd659f420e095c4
ethanchung936 <ethanchung936@gmail.com>
2024-02-23 17:40:20 -0700

    Added attributes to attendee and constructor

--- code/noRAM/app/src/main/java/com/example/noram/model/Attendee.java
+++ code/noRAM/app/src/main/java/com/example/noram/model/Attendee.java
@@ -5,13 +5,87 @@ package com.example.noram.model;
  */
 public class Attendee extends UserProfile {
     private int identifier;
+    private String firstName;
+    private String lastName;
+    // Phone number is stored as a string to avoid overflow and to deal with any character if necessary
+    private String phoneNumber;
 
     /**
      * A constructor to create an attendee
      * @param identifier the identifier of the attendee
+     * @param firstName the first name of the attendee
+     * @param lastName the last name of the attendee
+     * @param phoneNumber the phone number of the attendee
+     *
      */
-    public Attendee(int identifier) {
+    public Attendee(int identifier, String firstName, String lastName, String phoneNumber) {
         super(identifier);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * A method to get the identifier of the attendee
+     * @return the identifier of the attendee
+     */
+    public int getIdentifier() {
+        return identifier;
+    }
+
+    /**
+     * A method to set the identifier of the attendee
+     * @param identifier the identifier of the attendee
+     */
+    public void setIdentifier(int identifier) {
+        this.identifier = identifier;
+    }
+
+    /**
+     * A method to get the first name of the attendee
+     * @return the first name of the attendee
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * A method to set the first name of the attendee
+     * @param firstName the first name of the attendee
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * A method to get the last name of the attendee
+     * @return the last name of the attendee
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * A method to set the last name of the attendee
+     * @param lastName the last name of the attendee
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * A method to get the phone number of the attendee
+     * @return the phone number of the attendee
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * A method to set the phone number of the attendee
+     * @param phoneNumber the phone number of the attendee
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
--- code/noRAM/app/src/main/java/com/example/noram/model/Database.java
+++ code/noRAM/app/src/main/java/com/example/noram/model/Database.java
@@ -61,9 +61,9 @@ public class Database {
      * @param identifier the identifier of the attendee
      * @return true if the attendee is added, false otherwise
      */
-    public boolean addAttendee(int identifier) {
+    public boolean addAttendee(int identifier, String firstName, String lastName, String phoneNumber) {
         try {
-            attendeeRef.add(new Attendee(identifier)); // TODO: add the attendee properly
+            attendeeRef.add(new Attendee(identifier, firstName, lastName, phoneNumber)); // TODO: add the attendee properly
             return true;
         } catch (Exception e) {
             return false;
