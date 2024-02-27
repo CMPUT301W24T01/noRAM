5d6f8b81b29368f4df6e4ad558b5ca7c17718bb1
gdumouli <90409961+gdumouli@users.noreply.github.com>
2024-02-26 17:08:02 -0700

    Added attendee-announcements

---
+++ code/noRAM/app/src/main/java/com/example/noram/AttendeeAnnouncementsActivity.java
@@ -0,0 +1,22 @@
+package com.example.noram;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.noram.model.Event;
+
+public class AttendeeNotificationsActivity extends AppCompatActivity {
+    private static final String eventIDLabel = "eventID";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.attendee_announcements);
+
+        // retrieve corresponding event in database
+        int eventID = getIntent().getIntExtra(eventIDLabel,0);
+        // TODO: Actually query database to get corresponding event
+        event = new Event();
+    }
+}
