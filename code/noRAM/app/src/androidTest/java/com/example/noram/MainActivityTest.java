b536343a886891cc127c7b61c926e8987091ecc8
Semicarlin <carlin.boyles@gmail.com>
2024-04-03 23:11:37 -0600

    Tests fixed

--- code/noRAM/app/src/androidTest/java/com/example/noram/MainActivityTest.java
+++ code/noRAM/app/src/androidTest/java/com/example/noram/MainActivityTest.java
@@ -54,7 +54,7 @@ public class MainActivityTest {
      */
     @Test
     public void attendeeButtonTest() {
-        onView(withId(R.id.attendeeButton)).perform(click());
+        onView(withId(R.id.bottom_nav_attend_events)).perform(click());
 
         intended(hasComponent(AttendeeActivity.class.getName()));
     }
@@ -64,7 +64,7 @@ public class MainActivityTest {
      */
     @Test
     public void organizerButtonTest() {
-        onView(withId(R.id.organizerButton)).perform(click());
+        onView(withId(R.id.bottom_nav_organize_events)).perform(click());
 
         intended(hasComponent(OrganizerActivity.class.getName()));
     }
