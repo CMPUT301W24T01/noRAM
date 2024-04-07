265016b3d690c546d14ada949e9eda09ac1350c1
ethanchung936 <ethanchung936@gmail.com>
2024-04-07 13:28:57 -0600

    fixed dashboard header

--- code/noRAM/app/src/main/java/com/example/noram/OrganizerActivity.java
+++ code/noRAM/app/src/main/java/com/example/noram/OrganizerActivity.java
@@ -57,7 +57,7 @@ public class OrganizerActivity extends AppCompatActivity {
         navBar.setSelectedItemId(NAV_PROFILE);
         activeFragment = profileFragment;
         header = findViewById(R.id.organizer_activity_header_text);
-        header.setText(R.string.organizer_fragment_event_list_header);
+        header.setText(R.string.dashboard);
 
         // create fragments into the fragmentManager
         fragmentManager.beginTransaction()
